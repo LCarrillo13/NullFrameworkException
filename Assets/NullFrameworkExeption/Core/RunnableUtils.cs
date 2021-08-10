5ff0aeab4bdb568f86f2caa414b381ecd5356444
James Mills <jmills@tunacornstudios.com>
2021-08-10 14:40:53 +1000

    Updated RunnableUtils and MobileInputManager

--- Assets/NullFrameworkExeption/Core/RunnableUtils.cs
+++ Assets/NullFrameworkExeption/Core/RunnableUtils.cs
@@ -10,8 +10,9 @@ namespace NullFrameworkException
         /// Attempts to retrieve the runnable behaviour from the passed gameObject or its children.
         /// </summary>
         /// <param name="_runnable">The reference the runnable will be set to.</param>
+        /// <param name="_optional">Whether or not the runnable is optional</param>
         /// <param name="_from">The gameObject we are attempting to get a runnable from.</param>
-        public static bool Validate<TRunnable>(ref TRunnable _runnable, GameObject _from) 
+        public static bool Validate<TRunnable>(ref TRunnable _runnable, GameObject _from, bool _optional) 
             where TRunnable : IRunnable
         {
             // If the passed Runnable is already set, return true
@@ -44,8 +45,11 @@ namespace NullFrameworkException
                 }
             }
 
-            // The second parameter of Unity's debug.log is the object associated with the LogMessage
-            Debug.LogException(new NullReferenceException($"Component {typeof(TRunnable).Name} is not present in the hierarchy of gameObject {_from.name}."), _from);
+            if(!_optional)
+            {
+                // The second parameter of Unity's debug.log is the object associated with the LogMessage
+                Debug.LogException(new NullReferenceException($"Component {typeof(TRunnable).Name} is not present in the hierarchy of gameObject {_from.name}."), _from);
+            }
 
             return false;
         }
@@ -55,12 +59,13 @@ namespace NullFrameworkException
         /// </summary>
         /// <param name="_runnable">The runnable being setup.</param>
         /// <param name="_from">The gameObject the runnable is attached to.</param>
+        /// <param name="_optional">Whether or not the runnable is optional</param>
         /// <param name="_params">Any additional information the Runnable's setup function needs.</param>
-        public static bool Setup<TRunnable>(ref TRunnable _runnable, GameObject _from, params object[] _params)
+        public static bool Setup<TRunnable>(ref TRunnable _runnable, GameObject _from, bool _optional, params object[] _params)
             where TRunnable : IRunnable
         {
             // Validate the component, if we can, set it up and return true
-            if(Validate(ref _runnable, _from))
+            if(Validate(ref _runnable, _from, _optional))
             {
                 _runnable.Setup(_params);
                 return true;
@@ -75,12 +80,13 @@ namespace NullFrameworkException
         /// </summary>
         /// <param name="_runnable">The runnable being run.</param>
         /// <param name="_from">The gameObject the runnable is attached to.</param>
+        /// <param name="_optional">Whether or not the runnable is optional</param>
         /// <param name="_params">Any additional information the Runnable's run function needs.</param>
-        public static void Run<TRunnable>(ref TRunnable _runnable, GameObject _from, params object[] _params) 
+        public static void Run<TRunnable>(ref TRunnable _runnable, GameObject _from, bool _optional, params object[] _params) 
             where TRunnable : IRunnable
         {
             // Validate the component in case we didn't do it earlier
-            if(Validate(ref _runnable, _from))
+            if(Validate(ref _runnable, _from, _optional))
             {
                 _runnable.Run(_params);
             }
--- Assets/NullFrameworkExeption/Mobile/Input/MobileInputManager.cs
+++ Assets/NullFrameworkExeption/Mobile/Input/MobileInputManager.cs
@@ -1,22 +1,22 @@
-using UnityEngine;
-
 namespace NullFrameworkException.Mobile.InputHandling
 {
-    public class MobileInputManager : MonoBehaviour
+    public class MobileInputManager : MonoSingleton<MobileInputManager>
     {
         public JoystickInputHandler joystick;
+        public SwipeInputHandler swiping;
         
         // Start is called before the first frame update
         private void Start()
         {
-            joystick = FindObjectOfType<JoystickInputHandler>();
-            joystick.Setup(this);
+            RunnableUtils.Setup(ref joystick, gameObject, true, this);
+            RunnableUtils.Setup(ref swiping, gameObject, true);
         }
 
         // Update is called once per frame
         private void Update()
         {
-            joystick.Run();
+            RunnableUtils.Run(ref joystick, gameObject, true);
+            RunnableUtils.Run(ref swiping, gameObject, true);
         }
     }
 }
