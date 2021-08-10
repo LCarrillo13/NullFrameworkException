d21545edf735722b4dca51176de279660ae184fe
James Mills <jmills@tunacornstudios.com>
2021-08-10 13:34:21 +1000

    Runnables

--- Assets/NullFrameworkExeption/Core/MonoSingleton.cs
+++ Assets/NullFrameworkExeption/Core/MonoSingleton.cs
@@ -1,54 +1,53 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 namespace NullFrameworkException
 {
-    // the '<ExampleSingleton>' is updating the 'TSingletonType' to be ExampleSingleton
-    //public class ExampleSingleton : MonoSingleton<ExampleSingleton> { }
+	// the '<ExampleSingleton>' is updating the 'TSingletonType' to be ExampleSingleton
+	//public class ExampleSingleton : MonoSingleton<ExampleSingleton> { }
 
-    // Generic types are just a placeholder for a future classtype that we don't know about yet
-    public class MonoSingleton<TSingletonType> : MonoBehaviour 
-        where TSingletonType : MonoSingleton<TSingletonType> // This makes sure that SINGLETON_TYPE inherits from MonoSingleton
-    {
-        public static TSingletonType Instance
-        {
-            get
-            {
-                // The internal instance isn't set, so attempt to find it in the scene
-                if(instance == null)
-                {
-                    instance = FindObjectOfType<TSingletonType>();
-                    
-                    // No instance was found, so throw a NullReferenceException detailing what singleton caused the error
-                    if(instance == null)
-                    {
-                        // The 'typeof(TSingletonType).Name' shows the exact class name of the generic type
-                        // This line will also give us a stacktrace, showing where the call to the Instance was before it existed
-                        throw new NullReferenceException($"No objects of type: {typeof(TSingletonType).Name} was found.");
-                    }
-                }
+	// Generic types are just a placeholder for a future classtype that we don't know about yet
+	public class MonoSingleton<TSingletonType> : MonoBehaviour 
+		where TSingletonType : MonoSingleton<TSingletonType> // This makes sure that SINGLETON_TYPE inherits from MonoSingleton
+	{
+		public static TSingletonType Instance
+		{
+			get
+			{
+				// The internal instance isn't set, so attempt to find it in the scene
+				if(instance == null)
+				{
+					instance = FindObjectOfType<TSingletonType>();
 
-                return instance;
-            }
-        }
-        
-        private static TSingletonType instance = null;
+					// No instance was found, so throw a NullReferenceException detailing what singleton caused the error
+					if(instance == null)
+					{
+						// The 'typeof(TSingletonType).Name' shows the exact class name of the generic type
+						// This line will also give us a stacktrace, showing where the call to the Instance was before it existed
+						throw new NullReferenceException($"No objects of type: {typeof(TSingletonType).Name} was found.");
+					}
+				}
 
-        /// <summary>
-        /// Has the singleton been generated?
-        /// </summary>
-        public static bool IsSingletonValid() => instance != null;
+				return instance;
+			}
+		}
 
-        /// <summary>
-        /// Force the singleton instance to not be destroyed on scene load.
-        /// </summary>
-        public static void FlagAsPersistant() => DontDestroyOnLoad(Instance.gameObject);
+		private static TSingletonType instance = null;
 
-        /// <summary>
-        /// Finds the instance within the scene
-        /// </summary>
-        protected TSingletonType CreateInstance() => Instance;
-    }
+		/// <summary>
+		/// Has the singleton been generated?
+		/// </summary>
+		public static bool IsSingletonValid() => instance != null;
+
+		/// <summary>
+		/// Force the singleton instance to not be destroyed on scene load.
+		/// </summary>
+		public static void FlagAsPersistant() => DontDestroyOnLoad(Instance.gameObject);
+
+		/// <summary>
+		/// Finds the instance within the scene
+		/// </summary>
+		protected TSingletonType CreateInstance() => Instance;
+	}
 }
